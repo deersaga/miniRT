b19a174daaab25c32ed845c4a67876db76ce3ff4
deer <deersaga4@gmail.com>
2023-01-31 17:45:04 +0900

    WIP

--- src/minirt.h
+++ src/minirt.h
@@ -6,7 +6,7 @@
 /*   By: katakagi <katakagi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 12:26:35 by katakagi          #+#    #+#             */
-/*   Updated: 2023/01/31 16:42:04 by susami           ###   ########.fr       */
+/*   Updated: 2023/01/31 17:43:25 by katakagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,78 @@
 
 typedef struct s_img	t_img;
 typedef struct s_screen	t_screen;
+typedef struct s_material	t_material;
 typedef struct s_scene	t_scene;
+typedef struct s_shape	t_shape;
+typedef struct s_light_source	t_light_source;
+typedef enum e_shape_kind t_shape_kind;
+typedef enum e_light_kind t_light_kind;
 
-struct s_scene {};
+typedef struct s_proto	t_prot;
+
+struct s_material {};
+
+// struct s_proto {
+// 	char			*idetifier;
+// 	FLOAT			ambient_ratio;
+// 	t_vec			eye_position;
+// 	t_vec			look_at;
+// 	FLOAT			HFOV;
+// 	t_vec			light_pos;
+// 	FLOAT			light_intencity_ratio;
+// 	t_vec			center;
+// 	FLOAT			radius;
+// 	t_color			color;
+// };
+
+struct s_scene {
+	t_shape			shapes;
+	t_light_source	light_sources;
+	t_color			ambient_intesity;
+	FLOAT			air_refraction;
+	t_vec			eye_position;
+	t_vec			look_at;
+	FLOAT			screen_distance;
+	FLOAT			HFOV;
+};
+
+enum e_shape_kind {
+	SPHERE,
+	PLANE,
+	CYLINDER,
+};
+
+struct s_shape {
+	t_shape_kind	kind;
+	t_material		*material;
+	t_shape			*next;
+
+	//sphere, cylinder
+	t_vec			center;
+	FLOAT			radius;
+	// //cylinder
+	// FLOAT			height;
+	// t_vec			orientation; // (0, 1, 0)
+	// //plane
+	// t_vec			normal;
+	// t_vec			position;
+};
+
+enum e_light_kind {
+	POINT,
+	DIRECTIONAL,
+};
+
+struct s_light_source {
+	t_light_kind	kind;
+	t_fcolor		intencity;
+	t_light_source	*next;
+
+	//point light
+	t_vec			position;
+	//directional light
+	t_vec			direction;
+};
 
 struct s_img {
 	void	*mlx_ptr;
--- src/parse.c
+++ src/parse.c
@@ -4,14 +4,102 @@
 #include "minirt.h"
 #include "get_next_line.h"
 
+
+t_shape	get_shapes(void)
+{
+	t_shape		head;
+	t_shape		*shape;
+	
+	return ((t_shape){SPHERE, NULL, NULL, vec_new(3, 0, 25), 1.0})
+	//head.next = NULL;
+	// shape = &head;
+	// {
+	// 	shape = shape->next = shape_new(SPHERE);
+	// 	shape->center = vec_new(3, 0, 25);
+	// 	shape->radius = 1.0;
+	// 	shape->material = get_default_material(fcolor_new(0.69, 0, 0));
+	// }
+	// {
+	// 	shape = shape->next = shape_new(SPHERE);
+	// 	shape->center = vec_new(2, 0, 20);
+	// 	shape->radius = 1.0;
+	// 	shape->material = get_default_material(fcolor_new(0, 0.69, 0));
+	// }
+	// {
+	// 	shape = shape->next = shape_new(SPHERE);
+	// 	shape->center = vec_new(1, 0, 15);
+	// 	shape->radius = 1.0;
+	// 	shape->material = get_default_material(fcolor_new(0, 0, 0.69));
+	// }
+	// {
+	// 	shape = shape->next = shape_new(SPHERE);
+	// 	shape->center = vec_new(0, 0, 10);
+	// 	shape->radius = 1.0;
+	// 	shape->material = get_default_material(fcolor_new(0, 0.69, 0.69));
+	// }
+	// /*
+	// {
+	// 	shape = shape->next = shape_new(SPHERE);
+	// 	shape->center = vec_new(-1, 0, 5);
+	// 	shape->radius = 1.0;
+	// 	shape->material = get_default_material(fcolor_new(0.69, 0, 0.69));
+	// }
+	// */
+	// {
+	// 	shape = shape->next = shape_new(CYLINDER);
+	// 	shape->center = vec_new(-1, 0, 5);
+	// 	shape->radius = 1.0;
+	// 	shape->orientation = vec_new(-3.0, 4.0, -10.0);
+	// 	vec_normalize(shape->orientation);
+	// 	shape->height = 1.0;
+	// 	shape->material = get_default_material(fcolor_new(0.69, 0, 0.69));
+	// }
+	// {
+	// 	shape = shape->next = shape_new(PLANE);
+	// 	shape->center = vec_new(0, -1, 0);
+	// 	shape->normal = vec_new(0, 1, 0);
+	// 	shape->material = get_default_material(fcolor_new(0.69, 0.69, 0.69));
+	// }
+	//return (head.next);
+}
+
+t_light_source	get_light_sources(void)
+{
+	// t_light_source	head;
+	// t_light_source	*ls;
+
+	return ((t_light_source){POINT, new_color(1, 1, 1), NULL, vec_new(0, 5, 0), vec_new(0, 0, 0)});
+	// head.next = NULL;
+	// ls = &head;
+	// {
+	// 	ls = ls->next = {POINT, new_color(1, 1, 1), NULL, vec_new(0, 5, 0), vec_new(0, 0, 0)};
+	// 	ls->position = vec_new(-5, 5, -5);
+	// 	ls->intencity = fcolor_new(0.5, 0.5, 0.5);
+	// }
+	// return (head.next);
+}
+
+t_scene	*get_scene(void)
+{
+	t_scene	*scene;
+
+	scene = calloc(1, sizeof(*scene));
+	scene->shapes = get_shapes();
+	scene->light_sources = get_light_sources();
+	scene->eye_position = vec_new(0, 0, -5);
+	scene->look_at = vec_new(0, 0, 0);
+	scene->screen_distance = 5.0;
+	return (scene);
+}
+
 t_scene	parse(int argc, const char *argv[])
 {
-	t_scene	scene;
+	t_scene	*scene;
 	int	fd;
 	char	*tail;
 	char	*line;
 
-	scene = (t_scene){};
+	//scene = (t_scene){};
 	errno = 0;
 	if (argc != 2)
 		fatal_error("parse", "[Usage]: ./miniRT config_file");
@@ -21,20 +109,17 @@ t_scene	parse(int argc, const char *argv[])
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 		fatal_error("parse", NULL);
-	line = get_next_line(fd);
-	while (line)
-	{
-		if (!strcmp(line, "A 0.2 255,255,255\n"))
-			;
-		else if (!strcmp(line, "C  -50.0,0,20 0,0,1 70\n"))
-			;
-		else if (!strcmp(line, "sp 0.0,0.0,20.6 12.6 10,0,255\n"))
-			;
-		else
-			fatal_error("parse", line);
-		line = get_next_line(fd);
-	}
-
-	return (scene);
+	close(fd);
+	scene = get_scene();
+	return (*scene);
 }
 
+
+		// if (!strcmp(line, "A 0.2 255,255,255\n"))
+		// 	;
+		// else if (!strcmp(line, "C -50.0,0,20 0,0,1 70\n"))
+		// 	;
+		// else if (!strcmp(line, "sp 0.0,0.0,20.6 12.6 10,0,255\n"))
+		// 	;
+		// else
+		// 	fatal_error("parse", line);
