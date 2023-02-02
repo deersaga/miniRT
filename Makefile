de3ff4ec19f1609c2db47e4dee2c009828bbe873
Shun Usami <usatie@gmail.com>
2023-02-02 10:46:06 +0900

    Split ray_trace.c

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: katakagi <katakagi@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/31 12:25:57 by katakagi          #+#    #+#              #
-#    Updated: 2023/02/02 10:32:15 by susami           ###   ########.fr        #
+#    Updated: 2023/02/02 10:45:48 by susami           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,6 +23,7 @@ SRCS	=	$(SRC_DIR)/main.c\
 			$(SRC_DIR)/img.c\
 			$(SRC_DIR)/hooks.c\
 			$(SRC_DIR)/sphere.c\
+			$(SRC_DIR)/ray_trace.c\
 
 DEBUG_SRCS	= $(SRC_DIR)/destructor.c
 DEBUG_OBJS	= $(DEBUG_SRCS:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: katakagi <katakagi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 12:25:21 by katakagi          #+#    #+#             */
-/*   Updated: 2023/02/01 18:15:03 by katakagi         ###   ########.fr       */
+/*   Updated: 2023/02/02 10:45:08 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,73 +55,6 @@ t_ray	get_ray(t_camera *camera, int x, int y)
 				camera->eye_position);
 	ray_dir = vec_unit(ray_dir);
 	return ((t_ray){.origin = camera->eye_position, .direction = ray_dir});
-}
-
-t_lighting	lighting_at(t_vec pos, t_light_source light_source)
-{
-	t_lighting	lighting;
-
-	lighting.direction = vec_sub(light_source.position, pos);
-	lighting.distance = vec_length(lighting.direction);
-	lighting.direction = vec_unit(lighting.direction);
-	lighting.intensity = light_source.intensity;
-	return (lighting);
-}
-
-t_color	diffuse_light(t_scene *scene, t_hit_record *rec, t_lighting *lighting)
-{
-	t_color	color;
-	FLOAT	nldot;
-
-	nldot = vec_dot(lighting->direction, rec->normal);
-	nldot = clamp(nldot, 0, 1);
-	color = vec_mul(lighting->intensity, scene->sphere.diffuse_factor);
-	color = vec_mul(color, color_new(nldot, nldot, nldot));
-	return (color);
-}
-
-t_color	specular_light(t_scene *scene, const t_ray *ray, t_hit_record *rec, t_lighting *lighting)
-{
-	FLOAT		nldot;
-	FLOAT		vrdot;
-	FLOAT		vrdot_pow_alpha;
-	t_vec		reflec;
-	t_vec		b;
-	t_vec		view;
-	t_color		ki;
-
-	nldot = vec_dot(lighting->direction, rec->normal);
-	b = vec_scalar_mul(nldot * 2, rec->normal);
-	reflec = vec_sub(b, lighting->direction);
-	view = vec_scalar_mul(-1, ray->direction);
-	view = vec_unit(view);
-	vrdot = vec_dot(view, reflec);
-	if (nldot <= 0 || vrdot <= 0)
-		return (color_new(0, 0, 0));
-	vrdot_pow_alpha = pow(vrdot, scene->sphere.shineness);
-	ki = vec_mul(scene->sphere.specular_factor, lighting->intensity);
-	return (vec_mul(ki, color_new(vrdot_pow_alpha, vrdot_pow_alpha, vrdot_pow_alpha)));
-}
-
-t_color	ray_trace(const t_ray *r, t_scene *scene)
-{
-	t_vec			unit_direction;
-	FLOAT			t;
-	t_hit_record	rec;
-	t_color			ret_color;
-	t_lighting		lighting;
-
-	if (sphere_hit(&scene->sphere, r, 0, INFINITY, &rec))
-	{
-		ret_color = vec_mul(scene->ambient_intensity, scene->sphere.ambient_factor);
-		lighting = lighting_at(rec.p, scene->light_source);
-		ret_color = vec_add(ret_color, diffuse_light(scene, &rec, &lighting));
-		ret_color = vec_add(ret_color, specular_light(scene, r, &rec, &lighting));
-		return (ret_color);
-	}
-	unit_direction = vec_unit(r->direction);
-	t = 0.5 * (unit_direction.y + 1.0);
-	return (vec_add(vec_scalar_mul((1.0 - t), color_new(1.0, 1.0, 1.0)), vec_scalar_mul(t, color_new(0.5, 0.7, 1.0))));
 }
 
 void	draw_image(t_screen *screen, t_scene *scene)
