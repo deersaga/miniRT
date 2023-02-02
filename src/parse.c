67cad01871bf147e4c7d9b7017891ddb3dd011d3
deer <deersaga4@gmail.com>
2023-02-02 14:52:25 +0900

    add tokenizer WIP

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: katakagi <katakagi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 10:31:52 by susami            #+#    #+#             */
-/*   Updated: 2023/02/02 12:21:46 by susami           ###   ########.fr       */
+/*   Updated: 2023/02/02 14:51:13 by katakagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <string.h>
 #include <unistd.h>
 #include "minirt.h"
+#include "parse.h"
 
 t_element	*sphere_element_alloc(t_vec center, FLOAT diameter, t_color color)
 {
@@ -80,11 +81,65 @@ void	element_add(t_element *head, t_element *elem)
 	head->next = elem;
 }
 
+bool	consume_blank(char **rest, char *buff)
+{
+	bool	consumed;
+
+	consumed = false;
+	while (*buff == ' ')
+	{
+		buff++;
+		consumed = true;
+	}
+	*rest = buff;
+	return (consumed);
+
+}
+
+bool	is_identifier(const char *s)
+{
+	static const char	*identifiers[] = {"A", "C", "L", "sp"};
+	size_t				i;
+
+	i = 0;
+	while (i < sizeof(identifiers) / sizeof(*identifiers))
+	{
+		if (!memcmp(s, identifiers[i], strlen(identifiers[i])))
+			return (true);
+	}
+	return (false);
+}
+
+bool	is_number(const char *s)
+{
+	if (!isdigit(*s))
+		return (true);
+	return (false);
+}
+
+
+
+void	tokenize(char *buff, t_token *head)
+{
+	size_t	i;
+
+	i = 0;
+	while (*buff)
+	{
+		if (consume_blank(&buff, buff))
+			continue ;
+		
+	}
+}
+
+
 t_element	*parse(int argc, const char *argv[])
 {
 	t_element	head;
 	int			fd;
 	char		*tail;
+	char		buff[1000];
+	t_token		head;
 
 	errno = 0;
 	if (argc != 2)
@@ -95,6 +150,9 @@ t_element	*parse(int argc, const char *argv[])
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 		fatal_error("parse", NULL);
+	read(fd, buff, 999);
+	buff[999] = '\0';
+	tokenize(&buff, &head);
 	close(fd);
 	head.next = NULL;
 	element_add(&head, ambient_element_alloc(0.1, color_new(255, 255, 255)));
--- tmp.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int	main(void)
-{
-	printf("tan(pi / 2) = %f\n", tan(M_PI_2));
-	printf("1 / tan(pi / 2) = %f\n", 1 / tan(M_PI_2));
-}
